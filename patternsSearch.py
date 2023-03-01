d0927cee362ff549028b3e7585d9bf67d39bb4e5
gemadf <gemadf49@gmail.com>
2023-03-01 16:28:57 +0100

    Algoritmo guarda resultados en un txt. Se añaden los txt para minimo de ocurrencias 5 y 10 y txt que contiene solo patrones de longuitud mayor que 2

--- patternsSearch.py
+++ patternsSearch.py
@@ -3,31 +3,37 @@ import itertools
 import pandas as pd
 import string
 
+
 def readData():
     data = pd.read_excel("data_nervous_genes.xlsx")
-    sequences = data["protein_sequence"].head()
+    sequences = data["protein_sequence"].head(100)
 
     return sequences
 
+
 def buscar_patrones(sequences):
     all_patterns = dict()
-    pattern_ocurrence = 5
+    pattern_ocurrence = 10
     for protein in sequences:
         protein_len = len(protein)
         patterns = set()
-        for pattern in range(1, protein_len+1):
-            for i in range(protein_len - pattern + 1):
-                sub_seq = protein[i:i+pattern]
+        for pattern_length in range(2, protein_len + 1):
+            for pattern in range(protein_len - pattern_length + 1):
+                sub_seq = protein[pattern:pattern + pattern_length]
                 if protein.count(sub_seq) >= pattern_ocurrence:
                     patterns.add(sub_seq)
-                    all_patterns[protein] = patterns
 
-    for key, value in all_patterns.items():
-        print(f"Protein sequence: {key}, patterns: {value}")
+
+        list_ordered_patterns = sorted(patterns, key=len, reverse=True)
+        all_patterns[protein] = list_ordered_patterns
+
+
+    with open("100ProteinasOcurrence10.txt", "w") as archivo:
+        for key, value in all_patterns.items():
+            print(f"Protein sequence: {key}, patterns: {value}", file=archivo)
 
 
 if __name__ == "__main__":
-    sequences = ['ABCABCAABABABABAB', 'BCABCBCCC', 'CABCA', 'ACBCA', 'ABCABCABCABCABCBACB']
-    #sequences = readData()
+    #sequences = ['ABCABCAABABABABABABAB', 'BCABCBCCCCC', 'CABCA', 'ACBCAABCABCABCABCABCABCABCAC', 'ABCABCABCABCABCBACB', '']
+    sequences = readData()
     buscar_patrones(sequences)
-
